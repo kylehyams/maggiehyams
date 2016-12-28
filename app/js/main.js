1b3c3cda68e15c07d32b8aee290814837b409c1f
Kyle Hyams <hello@kylehya.ms>
2016-12-28 07:29:41 -0500

    Added fade-in animations

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,10 @@
-// Add animation class to body on load
+// Add class to the body once the page is loaded, to animate-in select objects
 $(function() {
 	$('body').addClass('animate');
 });
 
-// Scroll on page
+
+// Smooth scrolling for anchor links
 (function (jQuery) {
 	jQuery.mark = {
 		jump: function (options) {
@@ -32,4 +33,102 @@ $(function() {
 
 jQuery(function(){  
 	jQuery.mark.jump();
+});
+
+
+// Add class to fade-in content once its a certain amount of pixels from the bottom of the viewport
+$(document).ready(function() {
+  var element = document.getElementById("intro-desktop");
+  $(element).addClass('js-fade-element-hide');
+
+  $(window).scroll(function() {
+    if( $("#intro-desktop").length > 0 ) {
+      var elementTopToPageTop = $(element).offset().top;
+      var windowTopToPageTop = $(window).scrollTop();
+      var windowInnerHeight = window.innerHeight;
+      var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+      var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+      var distanceFromBottomToAppear = 300;
+
+      if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+        $(element).addClass('js-fade-element-show');
+      }
+      else if(elementTopToWindowBottom < 0) {
+        $(element).removeClass('js-fade-element-show');
+        $(element).addClass('js-fade-element-hide');
+      }
+    }
+  });
+});
+
+$(document).ready(function() {
+  var element = document.getElementsByClassName("js-fadeInElementA");
+  $(element).addClass('js-fade-element-hide');
+
+  $(window).scroll(function() {
+    if( $(".js-fadeInElementA").length > 0 ) {
+      var elementTopToPageTop = $(element).offset().top;
+      var windowTopToPageTop = $(window).scrollTop();
+      var windowInnerHeight = window.innerHeight;
+      var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+      var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+      var distanceFromBottomToAppear = 300;
+
+      if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+        $(element).addClass('js-fade-element-show');
+      }
+      else if(elementTopToWindowBottom < 0) {
+        $(element).removeClass('js-fade-element-show');
+        $(element).addClass('js-fade-element-hide');
+      }
+    }
+  });
+});
+
+$(document).ready(function() {
+  var element = document.getElementsByClassName("js-fadeInElementB");
+  $(element).addClass('js-fade-element-hide');
+
+  $(window).scroll(function() {
+    if( $(".js-fadeInElementB").length > 0 ) {
+      var elementTopToPageTop = $(element).offset().top;
+      var windowTopToPageTop = $(window).scrollTop();
+      var windowInnerHeight = window.innerHeight;
+      var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+      var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+      var distanceFromBottomToAppear = 300;
+
+      if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+        $(element).addClass('js-fade-element-show');
+      }
+      else if(elementTopToWindowBottom < 0) {
+        $(element).removeClass('js-fade-element-show');
+        $(element).addClass('js-fade-element-hide');
+      }
+    }
+  });
+});
+
+$(document).ready(function() {
+  var element = document.getElementsByTagName("footer");
+  $(element).addClass('js-fade-element-hide');
+
+  $(window).scroll(function() {
+    if( $("footer").length > 0 ) {
+      var elementTopToPageTop = $(element).offset().top;
+      var windowTopToPageTop = $(window).scrollTop();
+      var windowInnerHeight = window.innerHeight;
+      var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+      var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+      var distanceFromBottomToAppear = 150;
+
+      if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+        $(element).addClass('js-fade-element-show');
+      }
+      else if(elementTopToWindowBottom < 0) {
+        $(element).removeClass('js-fade-element-show');
+        $(element).addClass('js-fade-element-hide');
+      }
+    }
+  });
 });
